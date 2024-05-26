8a062dc8ef29123573fa14e6408372b48f61b0d0
fel1pe-r-s <1felipersilva1@gmail.com>
2024-05-26 20:43:46 -0300

    Finalizado telas da aplicação

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,16 +2,17 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{ts,tsx}",
+    "./src/components/**/*.{ts,tsx}",
+    "./src/app/**/*.{ts,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        sans: "var(--font-inter)",
+      },
+      gridTemplateRows: {
+        app: "min-content max-content",
       },
     },
   },
